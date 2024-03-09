000e344a603b811a2338eef724f7775a8ef54502
shruti dhankade <shrutidhankade@gmail.com>
2024-03-09 23:24:44 +0530

    co

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,13 +8,13 @@ export default function About() {
 		<div className="py-16  bg-[#F6F5F8] px-4 max-sm:w-full">
 			<div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6 ">
 				<div className="space-y-6 md:space-y-0  lg:space-y-0  sm:space-y-0 md:flex md:gap-6  ">
-					<div className="md:5/12 h-[95vh] max-sm:w-[100%] max-sm:h-[100vh] lg:w-[50%]  ml-10 max-sm:-ml-8 ">
+					<div className="md:5/12 h-[95vh] max-sm:w-[100%] max-sm:h-[80vh] max-sm:-mt-10 lg:w-[50%]  ml-10 max-sm:-ml-8 ">
 						<img className='w-full  h-full object-cover  '
 							src="https://static.wixstatic.com/media/2a1a02_b7200f82e5184dbeac5321d69e69789f~mv2.jpg/v1/fill/w_950,h_1088,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2a1a02_b7200f82e5184dbeac5321d69e69789f~mv2.jpg"
 							alt="image"
 						/>
 					</div>
-					<div className='w-[26%] max-sm:top-60  h-[63vh] max-sm:h-[52vh] max-sm:w-[75vw] bg-white absolute left-[30%] top-[35vh] max-sm:-ml-6 '>
+					<div className='w-[26%] max-sm:top-60  h-[63vh] max-sm:h-[51vh] max-sm:w-[75vw] bg-white absolute left-[30%] top-[35vh] max-sm:-ml-6 '>
 						<h1 className='text-[2.7vw] max-sm:text-3xl  font-bold leading-[3.1vw] tracking-wide  text-black ml-10 mt-10'>
 							Be Healthy, <br />
 							Be Happy</h1>
